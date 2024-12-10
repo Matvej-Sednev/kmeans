22d57cde8f35568d048e842c4b90e1cd0955d992
Matvej-Sednev <158525445+Matvej-Sednev@users.noreply.github.com>
2024-12-10 21:41:57 +0300

    Delete exam04.sql

--- exam04.sql
+++
@@ -1,4 +0,0 @@
-create schema exam04;
-set search_path = 'exam04';
-
-create table weather(month int, day int, temp_hi int, temp_lo int, feels_like int, weather_temp int, humidity int, pressure int,  )
